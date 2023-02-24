b40ced95b78698691bff13befd365274377c2014
Ju-yeop <dhwnduq12@naver.com>
2023-02-25 01:53:48 +0900

    DP4

--- DP/Arithmetic_operation.py
+++ DP/Arithmetic_operation.py
@@ -1,11 +1,18 @@
 def solution(arr):
-    answer = 0
+    minmax = [0, 0]
+    sum_value = 0
     for i in reversed(range(len(arr))):
-        if i % 2 == 0:
-            if arr[i-1] == "-":
-                answer = max(-(answer + int(arr[i])), answer - int(arr[i]))
-            else: answer += int(arr[i])
-    return answer
+        if arr[i] == "+":
+            continue
+        elif arr[i] == "-":
+            mi, mx = minmax
+            minmax[0] = min(-sum_value + mi, -(sum_value + mx))
+            minmax[1] = max(-int(arr[i+1]) + (sum_value - int(arr[i+1])) + mx, -(sum_value + mi))
+            sum_value = 0
+        else:
+            sum_value += int(arr[i])
+    minmax[1] += sum_value
+    return minmax[1]
 
 print(solution(["1", "-", "3", "+", "5", "-", "8"]))
 print(solution(["5", "-", "3", "+", "1", "+", "2", "-", "4"]))
